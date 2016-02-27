5cec9b5a50df30d53e114d38bf2e23dffbb95313
Yue Zhang <yue.jack.zhang@gmail.com>
2016-02-27 02:34:12 -0800

    packaging issues

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from Cython.Build import cythonize
 setup(
     name = 'frozenintervaltree',
     packages = ['frozenintervaltree'],
+    install_requires=['cython'],
     version = '0.1',
     description = 'Frozen interval trees',
     author = 'Yue Zhang',
     author_email = 'yjzhang@cs.washington.edu',
-    ext_modules = cythonize('src/frozenintervaltree.pyx')
+    ext_modules = cythonize('frozenintervaltree/frozenintervaltree.pyx')
 )
